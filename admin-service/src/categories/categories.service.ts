24772354ea7ce5494c5d5dcb404f5de6ab905ed3
luisfelipe03 <llipeandrade34@gmail.com>
2025-01-03 16:44:41 -0300

    feat: adicionado métodos de busca

--- admin-service/src/categories/categories.service.ts
+++ admin-service/src/categories/categories.service.ts
@@ -6,6 +6,8 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Repository } from 'typeorm';
 import { FileUploadException } from 'src/shared/errors/fileUploadException';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { CategoryNotFoundException } from './errors/categoryNotFoundException';
+import { NoCategoriesFoundException } from './errors/noCategoriesFoundException';
 
 @Injectable()
 export class CategoriesService {
@@ -50,5 +52,25 @@ export class CategoriesService {
       console.error('Error uploading image to S3:', error);
       throw new FileUploadException('Error uploading image to S3');
     }
+  }
+
+  async findAll() {
+    const categories = await this.categoryRepo.find();
+    if (categories.length === 0) {
+      throw new NoCategoriesFoundException();
+    }
+    return categories;
+  }
+
+  async findOne(id: string) {
+    const category = await this.categoryRepo.findOne({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new CategoryNotFoundException();
+    }
+
+    return category;
   }
 }
