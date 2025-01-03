49fd44a8b4039fec6a3401aa75be3b748dc9595c
luisfelipe03 <llipeandrade34@gmail.com>
2025-01-03 16:32:40 -0300

    feat: adicionando configurações de conexão com o banco de dados e pipes de validação

--- admin-service/src/app.module.ts
+++ admin-service/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from './shared/env';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      username: env.DB_USER,
+      password: env.DB_PASS,
+      database: env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
--- admin-service/src/main.ts
+++ admin-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from './shared/env';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(env.PORT);
 }
 bootstrap();
