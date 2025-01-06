8f76d5fb54a1c06164056107b246bd3e4ec353da
luisfelipe03 <llipeandrade34@gmail.com>
2025-01-06 14:44:50 -0300

    feat: Migrando a injeção de dependência do repositório para uma classe especifica

--- admin-service/src/categories/categories.module.ts
+++ admin-service/src/categories/categories.module.ts
@@ -3,11 +3,12 @@ import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { TypeORMCategoryRepository } from './repositories/typeORM/type-orm-category-repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, TypeORMCategoryRepository],
   exports: [CategoriesService],
 })
 export class CategoriesModule {}
